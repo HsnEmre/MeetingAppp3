ccd5145e80a71beadfb30691f76805d37adcdcbb
Hasan Emre Buzkan <92093161+HsnEmre@users.noreply.github.com>
2024-01-16 01:19:41 +0300

    devam2

--- MeetingApp3/Controllers/HomeController.cs
+++ MeetingApp3/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using MeetingApp3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MeetingApp3.Controllers
@@ -5,8 +6,27 @@ namespace MeetingApp3.Controllers
     public class HomeController:Controller
     {
         //Localhost / Home
-        public IActionResult Index(){
-            return View();
+        public IActionResult Index()
+        {
+                int saat= DateTime.Now.Hour;
+                
+
+                ViewBag.selamlama=saat>12?"iyi günler":"Günaydın";
+                ViewBag.UserName="Hasan Emre";
+
+                //viewbag alternatif
+                ViewData["Selamlama"]=saat>12?"iyi günler":"Günaydın";
+                ViewData["UserName"]="Hasan Emre Buzkan";
+
+                MeetingInfo meetingInfo=new MeetingInfo(){
+                    Id=1,
+                    Location="İzmir",
+                    Date=new DateTime(2024, 01 ,20 ,0 , 0),
+                    NumberOfPeople=100
+
+                };
+
+            return View(meetingInfo);
         }
 
     }
